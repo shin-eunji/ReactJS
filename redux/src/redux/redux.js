67d882b08c21d0a9705d38c8594d6bddec8c8c02
shineunji <dmswlckato@gmail.com>
2020-08-30 20:44:44 +0900

    reducer

---
+++ redux/src/redux/redux.js
@@ -0,0 +1,13 @@
+const initialState = {
+    openSidebar: false
+}
+
+function reducer(state= initialState, action) {
+    switch (action.type) {
+        default: return state;
+    }
+}
+/*
+const reducer*/
+
+export default reducer;

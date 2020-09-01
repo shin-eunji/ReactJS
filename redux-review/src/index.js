323c8f0999a396fe3df532d96074a41fe208cc31
shineunji <dmswlckato@gmail.com>
2020-09-01 12:57:07 +0900

    state, action

--- redux-review/src/index.js
+++ redux-review/src/index.js
@@ -4,6 +4,11 @@ import App from './App';
 import {Provider} from 'react-redux'
 import store from "./redux/store";
 
+console.log("store", store);
+
+const state = store.getState();
+console.log("state", state);
+
 ReactDOM.render(
   <React.StrictMode>
       <Provider store={store}>
--- redux-review/src/redux/user/redux.js
+++ redux-review/src/redux/user/redux.js
@@ -1,29 +1,28 @@
 const initialState = {
-    openSidebar: false,
-    popup: false
+    isLoggedIn: false,
+    info: {}
 }
 
 const Action = {
     Types: {
-        UPDATE_STATE: '@@APP/UPDATE_STATE'
+        UPDATE_STATE: '@@USER/UPDATE_STATE'
     },
 
     Creator: {
         updateState: (props) => ({
-            Type: Action.Types.UPDATE_STATE,
-            payload: props
-
+            type: Action.Types.UPDATE_STATE,
+            props
         })
     }
 }
 
-const reducer = (state= initialState, action) => {
+const reducer = (state=initialState, action) => {
     switch (action.type) {
-        default: return state;
+        default: return state
         case Action.Types.UPDATE_STATE: {
             return {
                 ...state,
-                ...action.props
+                ...action.type
             }
         }
     }

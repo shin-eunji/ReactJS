b3e8610bd7bef2a2da526d906f3631acb54a91d4
shineunji <dmswlckato@gmail.com>
2020-09-03 12:53:18 +0900

    checked

--- redux-review/src/App.js
+++ redux-review/src/App.js
@@ -1,29 +1,17 @@
 import React from 'react';
 import styled from 'styled-components';
-import Header from "./components/pages/Home/Header";
-import {connect} from "react-redux";
 
 function App (props) {
 
-    const {
-        state = {}
-    } = props;
+    const {} = props;
 
     return (
         <Container>
-            <Header/>
+            App
         </Container>
     )
 }
 
 const Container = styled.div`
-
 `
-
-const mapStateToProps = state => ({
-    state
-})
-const mapDispatchToProps = dispatch => ({
-    dispatch
-})
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
--- redux-review/src/redux/store.js
+++
@@ -1,7 +0,0 @@
-import { createStore } from "redux";
-import reducers from "./redux";
-
-
-const store = createStore(reducers)
-
-export default store;

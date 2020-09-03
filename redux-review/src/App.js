a11841adf147a386db4edb98cff8c21fcd785bee
shineunji <dmswlckato@gmail.com>
2020-09-03 11:37:33 +0900

    0903: 하면 할수록 할게 많아..

--- redux-review/src/App.js
+++ redux-review/src/App.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import styled from 'styled-components';
 
-
 function App (props) {
 
     const {} = props;
 
     return (
         <Container>
-            App
         </Container>
     )
 }
 
 const Container = styled.div`
+
 `
 export default App;
--- redux-review/src/index.js
+++ redux-review/src/index.js
@@ -1,29 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import {Provider} from 'react-redux'
-import store from "./redux/store";
-
-const state = store.getState();
-console.log("state", state);
-console.log("store", store);
-
-store.dispatch({
-    type: '@@APP/UPDATE_STATE',
-    prop: {
-        name: 'koo'
-    }
-})
-
-const state2 = store.getState();
-console.log("after dispatch Action", state2);
 
 
 ReactDOM.render(
   <React.StrictMode>
-      <Provider store={store}>
         <App />
-      </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );

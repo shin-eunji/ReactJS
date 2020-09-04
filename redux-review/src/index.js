6c2e128e80fe6af9beaadc3aecb0c4e9e6a29bab
shineunji <dmswlckato@gmail.com>
2020-09-04 23:28:56 +0900

    다시

--- redux-review/src/index.js
+++ redux-review/src/index.js
@@ -1,21 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { Provider } from 'react-redux'
-import store from "./redux/store";
-import './scss/style.scss'
-import { Router, Route } from 'react-router-dom'
-import { HISTORY } from "./Helpers/History";
 
 ReactDOM.render(
     <React.StrictMode>
-        <Provider store={store}>
-            <Router history={HISTORY}>
-                <Route>
-                    <App />
-                </Route>
-            </Router>
-        </Provider>
+        <App />
     </React.StrictMode>,
   document.getElementById('root')
 );

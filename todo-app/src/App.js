70fe8b01e9041b48f8cb0c46c09b6e83ee75f3ab
shineunji <dmswlckato@gmail.com>
2020-09-15 14:22:50 +0900

    todo

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,50 +1,17 @@
-import React, {useState} from 'react';
+import React from 'react';
 import styled from 'styled-components';
-import './scss/style.scss'
-import TodoTemplate from "./components/TodoTemplate";
-import TodoInsert from "./components/TodoInsert";
-import TodoList from "./components/TodoList";
 
-function App () {
+function App (props) {
 
-    const [todos, setTodos] = useState([
-        {
-            id: 1,
-            text: "Lorem ipsum dolor.",
-            checked: true
-        },
-        {
-            id: 2,
-            text: "Lorem ipsum dolor sit amet, consectetur.",
-            checked: true
-        },
-        {
-            id: 3,
-            text: "Lorem ipsum dolor sit amet.",
-            checked: true
-        },
-        {
-            id: 4,
-            text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum, vitae.",
-            checked: true
-        },
-    ])
+    const {} = props;
 
     return (
         <Container>
-            <TodoTemplate/>
-            <TodoInsert/>
-            <TodoList todos={todos}/>
+            App
         </Container>
     )
 }
 
 const Container = styled.div`
-  display:flex;
-  flex-direction:column;
-  justify-content:center;
-  align-items:center;
-  height: 100vh;
-  
 `
 export default App;
